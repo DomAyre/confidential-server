449ca0de977bf57f5eaa77d976655ddc134b12fd
Dominic Ayre <domayre@outlook.com>
2025-04-13 16:55:37 +0000

    Make a factory for creating the flask app

--- src/server/run.py
+++ src/server/run.py
@@ -1,13 +1,16 @@
 from flask import Flask
-from args import parse_args
+from server.args import parse_args
 from endpoints.fetch import fetch as _fetch
 
-app = Flask(__name__)
-args = parse_args()
+def create_app(args):
 
-@app.route('/fetch/<path:target>', methods=['GET'])
-def fetch(target):
-    return _fetch(target, args)
+    app = Flask(__name__)
+
+    @app.route('/fetch/<path:target>', methods=['GET'])
+    def fetch(target):
+        return _fetch(target, args)
+
+    return app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    create_app(parse_args()).run(debug=True)
