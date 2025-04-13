22a2dd6e08e780927cd3e25122a162e241f03bb6
Dominic Ayre <domayre@outlook.com>
2025-04-13 17:06:35 +0000

    Add tests for hosting directories

--- test/endpoints/test_fetch.py
+++ test/endpoints/test_fetch.py
@@ -1,7 +1,8 @@
 from argparse import Namespace
-import pytest
 from config.parser import parse_config_file
 from server.run import create_app
+import zipfile
+import io
 
 def get_test_client(config_path):
     return create_app(args=Namespace(
@@ -25,3 +26,13 @@ def test_requesting_file_not_in_config():
     server = get_test_client("examples/config/single_file_single_policy.yml")
     response = server.get('/fetch/license')
     assert response.status_code == 404
+
+
+def test_requesting_dir_in_config():
+    server = get_test_client("examples/config/single_dir_single_policy.yml")
+
+    response = server.get('/fetch/examples')
+    assert response.status_code == 200
+
+    zip_file = zipfile.ZipFile(io.BytesIO(response.data))
+    assert len(zip_file.namelist()) > 0
