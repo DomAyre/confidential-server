fb1b3fc6d1a52dbb58150c91f353abae0d291789
Dominic Ayre <domayre@outlook.com>
2025-04-16 12:22:55 +0000

    Make run.py document the API

--- src/server/run.py
+++ src/server/run.py
@@ -1,14 +1,22 @@
+from typing import cast
 from flask import Flask, request
+from config.parser import ServerConfig
+from encryption_wrapper.src.lib.b64_to_public_key import b64_to_public_key
 from server.args import parse_args
 from endpoints.fetch import fetch as _fetch
 
 def create_app(args):
 
     app = Flask(__name__)
+    config = cast(ServerConfig, args.config)
 
     @app.route('/fetch/<path:target>', methods=['POST'])
-    def fetch(target):
-        return _fetch(request, target, args)
+    def fetch(target: str):
+        return _fetch(
+            config,
+            target,
+            b64_to_public_key(request.json.get('wrapping_key')),
+        )
 
     return app
 
